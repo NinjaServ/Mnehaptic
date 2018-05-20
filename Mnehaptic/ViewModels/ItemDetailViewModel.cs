6e108a9acb9ae0a6699f1a9baac004e28830c28f
NinjaServ <urbachs@gmail.com>
2018-05-19 18:57:56 -0700

    Initial check-in of module Mnehaptic

---
+++ Mnehaptic/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mnehaptic.Models;
+
+namespace Mnehaptic.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
